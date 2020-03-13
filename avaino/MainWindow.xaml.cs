9a4b60a2ad038761959fcaa73581852f71efad2c
Twometer <twometer@outlook.de>
2020-03-13 13:42:36 +0100

    Parser

--- avaino/MainWindow.xaml.cs
+++ avaino/MainWindow.xaml.cs
@@ -27,14 +27,28 @@ namespace avaino
             InitializeComponent();
             ConfigureScintilla();
         }
-                                 
+
         private void Format()
         {
-            var content = Scintilla.Text;
-            
+            var reader = new StringReader(Scintilla.Text);
+            var writer = new StringWriter();
+            var indentLevel = 0;
+            while (reader.Peek() >= 0)
+            {
+                var line = reader.ReadLine();
+
+                var braces = 0;
+                foreach (var chr in line)
+                {
+                    if (chr == '{') braces++;
+                    else if (chr == '}') braces--;
+                }
+                if (braces >= 0) writer.WriteLine(new string(' ', indentLevel * 4) + line.Trim());
+                indentLevel += braces;
+                if (braces < 0) writer.WriteLine(new string(' ', indentLevel * 4) + line.Trim());
+            }
+            Scintilla.Text = writer.ToString();
         }
-
-
 
         public static System.Drawing.Color IntToColor(int rgb)
         {
@@ -94,6 +108,11 @@ namespace avaino
 
             Scintilla.SetKeywords(0, "class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
             Scintilla.SetKeywords(1, "void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET ICACHE_RAM_ATTR Serial");
+        }
+
+        private void FormatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Format();
         }
     }
 }
